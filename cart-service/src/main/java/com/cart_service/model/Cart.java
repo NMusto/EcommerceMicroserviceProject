d68ed72a41c8f6391352c940a560952601d9905f
Nelson Musto <mustonelson@gmail.com>
2025-06-05 10:01:27 -0300

    feat(cart): create Cart and CartItem entities

---
+++ cart-service/src/main/java/com/cart_service/model/Cart.java
@@ -0,0 +1,32 @@
+package com.cart_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "total_amount")
+    private Double totalAmount;
+
+}
---
+++ cart-service/src/main/java/com/cart_service/model/CartItem.java
@@ -0,0 +1,35 @@
+package com.cart_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    private Integer quantity;
+
+    @Column(name = "unit_price")
+    private Double unitPrice;
+
+    private Double subtotal;
+
+}
