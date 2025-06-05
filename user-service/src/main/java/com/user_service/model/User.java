7468debc844393f1609bfcd6d449605ae3303ebb
Nelson Musto <mustonelson@gmail.com>
2025-06-05 10:48:08 -0300

    feat(user): create User entity

---
+++ user-service/src/main/java/com/user_service/model/User.java
@@ -0,0 +1,25 @@
+package com.user_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+    private String phone;
+    private String email;
+}
