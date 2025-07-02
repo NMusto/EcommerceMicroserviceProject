daa15cc5c33c6f782a8d0f013d76f1f0780db864
Nelson Musto <mustonelson@gmail.com>
2025-07-02 17:00:41 -0300

    refactor(inventory-service): refactor Kafka trusted packages

--- inventory-service/src/main/java/com/inventory_service/service/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/inventory_service/service/InventoryServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class InventoryServiceImpl {
+public class InventoryServiceImpl implements InventoryService{
 
     private final InventoryRepository inventoryRepository;
 
