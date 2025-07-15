42db0700d773edd02a1a3e34116c3653d0dd6dba
Nelson Musto <mustonelson@gmail.com>
2025-07-15 11:38:27 -0300

    feat(order-service): create OrderResponse

---
+++ order-service/src/main/java/com/order_service/order/dto/OrderResponse.java
@@ -0,0 +1,28 @@
+package com.order_service.order.dto;
+
+import com.order_service.order.entity.OrderState;
+import com.order_service.orderitem.entity.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponse {
+
+    private Long id;
+    private Long userId;
+    private LocalDate createdAt;
+    private OrderState orderState;
+    private String shippingAddress;
+    private String shippingCity;
+    private String shippingPostalCode;
+    private List<OrderItem> items;
+    private Double totalAmount;
+}
