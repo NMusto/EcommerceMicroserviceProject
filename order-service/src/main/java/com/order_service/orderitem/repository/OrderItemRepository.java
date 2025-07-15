e7d99b1be24b8215a0f7e4f977c58d400c043583
Nelson Musto <mustonelson@gmail.com>
2025-07-15 10:50:24 -0300

    feat(order-service): create OrderItemRepository

---
+++ order-service/src/main/java/com/order_service/orderitem/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.order_service.orderitem.repository;
+
+import com.order_service.orderitem.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
