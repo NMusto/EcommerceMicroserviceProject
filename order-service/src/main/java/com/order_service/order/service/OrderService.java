310ce57ac5f3adffa23538b8a6c759608f000587
Nelson Musto <mustonelson@gmail.com>
2025-07-15 11:54:31 -0300

    feat(order-service): create OrderService and OrderServiceImpl initial structure

--- order-service/src/main/java/com/order_service/order/service/OrderService.java
+++ order-service/src/main/java/com/order_service/order/service/OrderService.java
@@ -1,5 +1,19 @@
 package com.order_service.order.service;
 
+import com.order_service.order.dto.OrderRequest;
+import com.order_service.order.dto.OrderResponse;
+import com.order_service.order.dto.OrderUpdateRequest;
+import com.order_service.order.dto.OrderUpdateState;
+
+import java.util.List;
+
 public interface OrderService {
 
+    List<OrderResponse> getAllOrders();
+    OrderResponse getOrderById(Long orderId);
+    OrderResponse getOrderByUserId(Long userId);
+    OrderResponse createOrder(OrderRequest orderRequest);
+    OrderResponse updateOrder(Long orderId, OrderUpdateRequest orderUpdateRequest);
+    OrderResponse updateOrderState(Long orderId, OrderUpdateState orderUpdateState);
+    String deleteOrder(Long orderId);
 }
---
+++ order-service/src/main/java/com/order_service/order/service/OrderServiceImpl.java
@@ -0,0 +1,49 @@
+package com.order_service.order.service;
+
+import com.order_service.order.dto.OrderRequest;
+import com.order_service.order.dto.OrderResponse;
+import com.order_service.order.dto.OrderUpdateRequest;
+import com.order_service.order.dto.OrderUpdateState;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public List<OrderResponse> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public OrderResponse getOrderById(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public OrderResponse getOrderByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public OrderResponse createOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    @Override
+    public OrderResponse updateOrder(Long orderId, OrderUpdateRequest orderUpdateRequest) {
+        return null;
+    }
+
+    @Override
+    public OrderResponse updateOrderState(Long orderId, OrderUpdateState orderUpdateState) {
+        return null;
+    }
+
+    @Override
+    public String deleteOrder(Long orderId) {
+        return null;
+    }
+}
