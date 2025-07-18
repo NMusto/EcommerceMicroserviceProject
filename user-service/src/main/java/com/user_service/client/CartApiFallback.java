334c9f93cc2f92b4cdac30eac6e3d961d0020fb7
Nelson Musto <mustonelson@gmail.com>
2025-07-18 18:42:42 -0300

    feat(user-service): create CartServiceUnavailableException and implement CartApiFallback

--- user-service/src/main/java/com/user_service/client/CartApiFallback.java
+++ user-service/src/main/java/com/user_service/client/CartApiFallback.java
@@ -1,6 +1,7 @@
 package com.user_service.client;
 
 import com.user_service.client.dto.CartRequest;
+import com.user_service.exception.CartServiceUnavailableException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,6 @@ public class CartApiFallback implements CartApi{
 
     @Override
     public void createCart(CartRequest cartRequest) {
-
+        throw new CartServiceUnavailableException("Cart service is unavailable. Cannot create cart for user ID: " + cartRequest.userId());
     }
 }
