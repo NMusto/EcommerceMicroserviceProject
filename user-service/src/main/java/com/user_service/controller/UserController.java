74d75dae396d59e7bf9bb2776ac53be65c855b75
Nelson Musto <mustonelson@gmail.com>
2025-07-18 12:09:30 -0300

    feat(user-service): create UserService, UserServiceImpl and UserController structures

---
+++ user-service/src/main/java/com/user_service/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.user_service.controller;
+
+import com.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+}
---
+++ user-service/src/main/java/com/user_service/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.user_service.service;
+
+import com.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+
+}
