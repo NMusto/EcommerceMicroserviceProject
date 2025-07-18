e9ed5c672acfa92064d88b7085ad2fbdf8d35bd0
Nelson Musto <mustonelson@gmail.com>
2025-07-18 17:35:25 -0300

    feat(cart-service): create ProductServiceUnavailableException and implement ProductApiFallBack

---
+++ cart-service/src/main/java/com/cart_service/client/ProductApiFallback.java
@@ -0,0 +1,12 @@
+package com.cart_service.client;
+
+import com.cart_service.client.dto.ProductApiResponse;
+import com.cart_service.exception.ProductServiceUnavailableException;
+
+public class ProductApiFallback implements ProductApi{
+
+    @Override
+    public ProductApiResponse getProductById(String productId) {
+        throw new ProductServiceUnavailableException("Product service is unavailable. Failed to get product with ID: " + productId);
+    }
+}
