4fcc128a4c33b307863d86751421a16a8de00c81
Nelson Musto <mustonelson@gmail.com>
2025-07-20 10:54:26 -0300

    feat(user-service): create getAllUsers and getUserById endpoints

--- user-service/src/main/java/com/user_service/controller/UserController.java
+++ user-service/src/main/java/com/user_service/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.user_service.controller;
 
+import com.user_service.dto.UserResponse;
 import com.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +19,15 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> users = userService.getAllUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long userId) {
+        UserResponse userResponse = userService.getUserById(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+    }
 }
